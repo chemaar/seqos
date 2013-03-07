30aaeb9dac3c44db262fcbba686bb0dd62a3fae7
Jose María Alvarez <jmalvarez@seerc.org>
2013-03-07 09:51:06 +0200

    Refactoring method

--- prototypes/relatweet/src/main/java/org/seerc/relate/relatweet/naive/TweetTimelineObservable.java
+++ prototypes/relatweet/src/main/java/org/seerc/relate/relatweet/naive/TweetTimelineObservable.java
@@ -7,48 +7,46 @@ import twitter4j.Status;
 import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
 
-//FIXME: Refactor methods
+
 public class TweetTimelineObservable extends Observable implements StatusListener  {
 
 	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
 		String msg = "Got a status deletion notice id:" + statusDeletionNotice.getStatusId();
-		setChanged();
-		notifyObservers(msg);
+		sendNotification(msg);
 
 	}
 
 	public void onStallWarning(StallWarning warning) {
 		String msg = "Got stall warning:" + warning;
-		setChanged();
-		notifyObservers(msg);
+		sendNotification(msg);
 
 	}
 
 	public void onStatus(Status status) {
 		String msg = "@" + status.getUser().getScreenName() + " - " + status.getText();
-		setChanged();
-		notifyObservers(msg);
+		sendNotification(msg);
 
 	}
 
 	public void onException(Exception e) {
 		String msg = e.getMessage();
-		setChanged();
-		notifyObservers(msg);
+		sendNotification(msg);
 
 	}
 
 	public void onScrubGeo(long userId, long upToStatusId) {		
 		String msg = "Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId;
-		setChanged();
-		notifyObservers(msg);
+		sendNotification(msg);
 
 	}
 
 	public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
 		String msg = "Got track limitation notice:" + numberOfLimitedStatuses;
+		sendNotification(msg);
+	}
+
+	private void sendNotification(String msg) {
 		setChanged();
 		notifyObservers(msg);
 	}
-
 }
