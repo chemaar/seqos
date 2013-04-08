79e39072a6d367b72b79b8b81f0762fa28310ecc
Jose María Alvarez <jmalvarez@seerc.org>
2013-04-08 13:43:51 +0300

    Adding examples of counting

---
+++ prototypes/mapreduce-intro/src/main/java/org/seerc/relate/mapreduce/examples/MRDPUtils.java
@@ -0,0 +1,12 @@
+package org.seerc.relate.mapreduce.examples;
+
+import java.util.Map;
+
+public class MRDPUtils {
+
+	public static Map<String, String> parse(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
